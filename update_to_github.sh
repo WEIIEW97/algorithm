497917f71766d5acea9d08945d5fed2002f77fc7
williamwei <williamwei9707@gmail.com>
2023-09-14 09:24:59 +0800

    add linux/macos shell support

---
+++ update_to_github.sh
@@ -0,0 +1,16 @@
+curdir=${pwd}
+cd $curdir
+
+git add .
+git status
+
+if [ -z "$1" ]; then
+  echo "Please provide a commit message."
+  exit 1
+fi
+
+git commit -m $1
+
+git push
+
+echo "pushing to remote repository done!"
