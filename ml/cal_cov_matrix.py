2109214bdb241655f4a7c37bc72e051708e333cc
william <williamwei9707@gmail.com>
2024-12-06 18:37:36 +0800

    add ml-folder

---
+++ ml/cal_cov_matrix.py
@@ -0,0 +1,7 @@
+"""
+Write a Python function that calculates the covariance matrix from a list of vectors. Assume that the input list represents a dataset where each vector is a feature, and vectors are of equal length.
+"""
+
+
+def calculate_covariance_matrix(vectors: list[list[float]]) -> list[list[float]]:
+    return covariance_matrix
