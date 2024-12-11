c40eb9f49f2fdf2f993f1f3273c9d21170137b64
william <williamwei9707@gmail.com>
2024-12-11 21:53:17 +0800

    add more ml problems.

--- ml/cal_cov_matrix.py
+++ ml/cal_cov_matrix.py
@@ -4,19 +4,22 @@ Write a Python function that calculates the covariance matrix from a list of vec
 
 
 def calculate_covariance_matrix(vectors: list[list[float]]) -> list[list[float]]:
-    n=len(vectors)
-    m=len(vectors[0])
+    n = len(vectors)
+    m = len(vectors[0])
 
-    mus=[sum(v) / m for v in vectors]
+    mus = [sum(v) / m for v in vectors]
     cov = [[0 for _ in range(n)] for _ in range(n)]
-    
+
     for i in range(n):
         for j in range(i, n):
-            c = sum((vectors[i][k]-mus[i])*(vectors[j][k]-mus[j]) for k in range(m)) / (m-1)
-            cov[i][j]=cov[j][i]=c
+            c = sum(
+                (vectors[i][k] - mus[i]) * (vectors[j][k] - mus[j]) for k in range(m)
+            ) / (m - 1)
+            cov[i][j] = cov[j][i] = c
     return cov
 
 
 if __name__ == "__main__":
     vectors = [[1, 2, 3], [4, 5, 6]]
     print(calculate_covariance_matrix(vectors))
+  
